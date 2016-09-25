1faf6d050bd70defa4a53ba6b45b8e7b395bcde3
brunomergh <Mergh.bruno@gmail.com>
2016-09-25 01:46:42 -0300

    funcao js

--- js/validador.js
+++ js/validador.js
@@ -7,7 +7,27 @@ function validarsenhas() {
 }
 
 function efetuarlogin() {
-    if(document.form1.login.value != "" && document.form1.Senha.value != "" && document.form1.Senha.value == document.form1.ConfirmarSenha.value){
+    if(document.form1.login.value != "" && document.form1.Senha.value != ""){
 		alert("Bem vindo, " + document.form1.login.value)
 	}
+}
+
+
+function clonarLinha(){
+	var row = document.getElementById("linhaParaClonar");
+	var table = document.getElementById("tabela");
+	var clone = row.cloneNode(true);
+	clone.id = "linhaClonada";
+	table.appendChild(clone);
+}
+
+function deleteRow(i){
+	document.getElementById("tabela").deleteRow(i)
+}
+
+function UsuarioConvidado(){
+	if(document.form1.email.value != ""){
+		alert("Email enviado!");
+	}
+	
 }
