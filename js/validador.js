39c23a802d42ba84f920cd1093adaad763a8e622
brunomergh <Mergh.bruno@gmail.com>
2016-09-24 22:47:06 -0300

    add link meuPerfil.html

--- js/validador.js
+++ js/validador.js
@@ -7,7 +7,7 @@ function validarsenhas() {
 }
 
 function efetuarlogin() {
-    if(document.form1.login.value != "" && document.form1.Senha.value != "" ){
+    if(document.form1.login.value != "" && document.form1.Senha.value != "" && document.form1.Senha.value == document.form1.ConfirmarSenha.value){
 		alert("Bem vindo, " + document.form1.login.value)
 	}
 }
